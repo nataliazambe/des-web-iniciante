2efcdf842ea35bc852c3ca4ef3284e57fa12cf68
Natalia Zambe <70557235+nataliazambe@users.noreply.github.com>
2024-12-08 19:32:47 -0300

    style: comments removed

--- livraria/livraria.js
+++ livraria/livraria.js
@@ -1,62 +1,7 @@
 var read = require("readline-sync");
-//var userName = read.question("May I have your name? ");
 
-//console.log(userName);
-
-/*var vetor = ["Nanana", "Moranguete", "Cerejaj"];
-console.log(vetor.length); // conta a quantidade de itens*/
-
-//function adicao()*/
-
-// Função criar objeto
-
-/*function criar_obj(id, nome, ano) {
-  console.log("Id: " + id, "/nNome: " + nome, "/nAno: " + ano);
-}*/
-
-//Objetos = "super variáveis" que armazenam uma "coleção de valores"
-//referenciados por nome
-
-//Como criar um objeto (notacao literal)
-
-/*function add(id, name, year) {
-  var book = {
-    id: id,
-    nome: name,
-    ano: year,
-  };
-  vetor.push(book);
-}*/
-
-// Funções tradicionais
-
-/* function find(id) {
-  for (var i = 0; i < vetor.length; i++) {
-    if (vetor[i].id == id) {
-      console.log(vetor[i]);
-    }
-  }
-}*/
-
-/*function remove(id) {
-  for (var i = 0; i < vetor.length; i++) {
-    if (vetor[i].id == id) {
-      vetor.splice(i, 2);
-    }
-  }
-}
-*/
-
-/*function show() {
-  for (var i = 0; i < vetor.length; i++) {
-    console.log(vetor[i]);
-  }
-}*/
-
-// Criando vetor global
 var vetor = [];
 
-//Arrow function
 const add = (id, name, year) => {
   var book = {
     id: id,
@@ -72,7 +17,6 @@ add(2);
 add(3, "Iu", 2004);
 add(5, "Fobo", 1312);
 add(6, "G1", 2024);
-//console.log(vetor);
 
 const find = (id) => {
   for (var i = 0; i < vetor.length; i++) {
@@ -82,8 +26,6 @@ const find = (id) => {
   }
 };
 
-//find(300);
-
 const remove = (id) => {
   for (var i = 0; i < vetor.length; i++) {
     if (vetor[i].id == id) {
@@ -91,16 +33,12 @@ const remove = (id) => {
     }
   }
 };
-//remove(1);
 
 const show = () => {
   for (var i = 0; i < vetor.length; i++) {
     console.log(vetor[i]);
   }
 };
-
-//show();
-//console.log(vetor);
 
 function menu(opt) {
   var opt = -1;
